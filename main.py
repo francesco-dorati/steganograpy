87c2cc61eddd9cc70991f775356693e047cf2499
Francesco Dorati <fd.francescodorati@gmail.com>
2021-08-19 23:51:48 +0200

    Implement Settings

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import sys
 import os
 import imghdr
 import time
+
 from getopt import getopt
 
 from steganography import Steganography
 
 ACCEPTED_FORMATS = ['jpeg', 'png']
+
+s = Steganography()
 
 def main():
     action = None
@@ -14,35 +17,70 @@ def main():
     message = None
     password = None
 
-    s = Steganography()
-
     if len(sys.argv) > 1:
         action = sys.argv[1].lower()
-        if action not in ['encode', 'decode']:
-            print_help()
-            exit(1)
+        if action in ['encode', 'decode']:
+            opts, _ = getopt(sys.argv[2:], "i:f:m:p:b:e:")
 
-        opts, _ = getopt(sys.argv[2:], "i:f:m:p:")
+            for opt in opts:
+                if opt[0] == '-i':
+                    img_path = opt[1]
+                    if not is_valid_image(img_path):
+                        exit()
 
-        for opt in opts:
-            if opt[0] == '-i':
-                img_path = opt[1]
-                if not is_valid_image(img_path):
-                    exit()
+                elif opt[0] == '-f':
+                    path = opt[1]
+                    if not is_valid_textfile(path):
+                        exit()
+                    with open(path, 'r') as file:
+                        message = file.read()
 
-            elif opt[0] == '-f':
-                path = opt[1]
-                if not is_valid_textfile(path):
-                    exit()
+                elif opt[0] == '-m':
+                    message = opt[1]
 
-                with open(path, 'r') as file:
-                    message = file.read()
+                elif opt[0] == '-p':
+                    password = opt[1]
 
-            elif opt[0] == '-m':
-                message = opt[1]
+                elif opt[0] == '-b':
+                    bits = opt[1]
+                    if bits.isnumeric() and int(bits) <= 8:
+                        s.BITS = int(bits)
+                    else:
+                        print('\033[0;31mInvalid bits number.\033[0m')
+                        exit()
 
-            elif opt[0] == '-p':
-                password = opt[1]
+                elif opt[0] == '-e':
+                    padding = opt[1]
+                    if padding.isnumeric():
+                        s.EOF_LENGTH = int(padding)
+                    else:
+                        print('\033[0;31mInvalid padding length.\033[0m')
+                        exit()
+
+        else:
+            action = None
+            opts, _ = getopt(sys.argv[1:], "b:e:")
+            for opt in opts:
+                if opt[0] == '-b':
+                    bits = opt[1]
+                    if bits.isnumeric() and int(bits) <= 8:
+                        s.BITS = int(bits)
+                    else:
+                        print('\033[0;31mInvalid bits number.\033[0m')
+                        exit()
+                        
+                elif opt[0] == '-e':
+                    padding = opt[1]
+                    if padding.isnumeric():
+                        s.EOF_LENGTH = int(padding)
+                    else:
+                        print('\033[0;31mInvalid padding length.\033[0m')
+                        exit()
+
+            if not bits and not padding:
+                print_help()
+                exit()
+            
         
     print_title()
 
@@ -88,21 +126,75 @@ def choose_action() -> str:
     Returns:
         action (str): the action chosen by the user.
     """
-    print("""
+    while True:
+        print("""
 \033[1mChoose action:\033[0m
     [1]\033[0;3m Encode\033[0m
     [2]\033[0;3m Decode\033[0m
+    [3]\033[0;3m Settings\033[0m
     [0]\033[0;3m Exit\033[0m
-""")
-    
-    a = None
-    while a not in ['1', '2', '0']:
-        a = input('> ')
+    """)
+        
+        a = None
+        while a not in ['1', '2', '3', '0']:
+            a = input('> ')
 
-    if a == '0':
-        exit()
+        if a == '0':
+            exit()
+        elif a == '1':
+            return 'encode'
+        elif a == '2':
+            return 'decode'
+        elif a == '3':
+            settings()
 
-    return 'encode' if a == '1' else 'decode'
+
+def settings():
+    """Displays settings."""
+    while True:
+        print("""
+\033[1mSettings:\033[0m
+    [1]\033[0;3m Change number of bits\033[0m
+    [2]\033[0;3m Change padding size\033[0m
+    [0]\033[0;3m Exit\033[0m
+            """)
+
+        s = None
+        while s not in ['1', '2', '0']:
+            s = input('> ')
+
+        if s == '0':
+            exit()
+        elif s == '1':
+            choose_bits()
+            break
+        elif s == '2':
+            choose_padding()
+            break
+
+def choose_bits():
+    """Asks the user for the number of bits reserved to the hidden message and changes it."""
+    while True:
+        n = input("\n\033[1mNumber of bits reserved to the hidden message: \033[0;2m(default is 2; maximum 8; 0 to exit)\033[0m ")
+
+        if n == '0':
+            exit()
+        elif n.isnumeric() and int(n) <= 8:
+            s.BITS = int(n)
+            print('\033[0;32mNumber of bits reserved to the hidden message updated successfully!\033[0m')
+            break
+
+def choose_padding():
+    """Asks the user for the length of padding and changes it."""
+    while True:
+        n = input("\n\033[1mNumber zeros in message padding: \033[0;2m(default is 16; 0 to exit)\033[0m ")
+
+        if n == '0':
+            exit()
+        elif n.isnumeric():
+            s.EOF_LENGTH = int(n)
+            print('\033[0;32mNumber zeros in message padding updated successfully!\033[0m')
+            break
 
 def choose_image(accepted_formats: list) -> str:
     """Asks the image path to the user.
@@ -245,12 +337,20 @@ def choose_password(message: str) -> str:
 
 def print_help():
     """Prints the help message."""
-    print("""
+    print(f"""
 Steganography
 
     Usage:
-        steganograpy [encode [-i image] [-f textfile | -m message] [-p password]]
-        steganograpy [decode [-i image] [-p password]]
+        {sys.argv[0]} [encode [-i image] [-f textfile | -m message] [-p password] [-b bits] [-e padding]]
+        {sys.argv[0]} [decode [-i image] [-p password] [-b bits] [-e padding]]
+
+    Args:
+        -i image        Image path to be encoded/decoded.
+        -f textfile     Textfile (.txt) to be hidden.
+        -m message      The mesage to be hidden.
+        -p password     The password to encrypt/decrypt the hidden message.
+        -b bits         The number of image pixel bits reserved for the hidden message.
+        -e padding      The end-of-message padding (number of zeroes).
     """)
 
 def print_title():
